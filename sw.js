9d898849dc8fbbea04cd713bbbe8873f9f1c9efc
fklaas <njwnrvwbv5@privaterelay.appleid.com>
2026-07-22 14:18:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="paris-reise-v4";
+const CACHE="paris-reise-v5";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./carte-food-perruche-summer.pdf","./Elio.pdf","./menu-perruche-1.jpg","./menu-perruche-2.jpg","./menu-perruche-3.jpg","./menu-perruche-4.jpg","./menu-perruche-5.jpg","./menu-perruche-6.jpg","./menu-elio-1.jpg","./menu-elio-2.jpg","./menu-elio-3.jpg","./menu-elio-4.jpg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
