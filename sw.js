c91783ca7326e9c410527c787a16122581c5347d
fklaas <njwnrvwbv5@privaterelay.appleid.com>
2026-07-22 15:42:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="paris-reise-v12";
+const CACHE="paris-reise-v13";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./carte-food-perruche-summer.pdf","./Elio.pdf","./menu-perruche-1.jpg","./menu-perruche-2.jpg","./menu-perruche-3.jpg","./menu-perruche-4.jpg","./menu-perruche-5.jpg","./menu-perruche-6.jpg","./menu-elio-1.webp","./menu-elio-2.webp","./menu-elio-3.webp","./menu-elio-4.webp","./fotospot-trocadero.webp","./fotospot-alexandre.webp","./fotospot-louvre.webp","./fotospot-disney.webp","./fotospot-universite.webp","./fotospot-seine.webp"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
