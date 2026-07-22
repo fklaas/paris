cc6174b5d5b692757fe5fb16e9a8c0c95c909301
fklaas <njwnrvwbv5@privaterelay.appleid.com>
2026-07-22 14:40:56 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,10 +1,11 @@
 GitHub Pages:
 1. ZIP entpacken.
-2. Alle Dateien gemeinsam in das Hauptverzeichnis des Repositories hochladen.
+2. Alle enthaltenen Dateien gemeinsam in das Hauptverzeichnis des Repositories hochladen.
 3. Vorhandene Dateien ersetzen.
-4. Nach dem Commit Strg+F5 drücken.
-5. Bei einer installierten PWA die App vollständig schließen und erneut öffnen.
+4. Alte Dateien menu-elio-1.jpg bis menu-elio-4.jpg können gelöscht werden.
+5. Die neuen Dateien menu-elio-1.webp bis menu-elio-4.webp müssen hochgeladen werden.
+6. Nach dem Commit Strg+F5 drücken.
+7. Bei einer installierten PWA die App vollständig schließen und erneut öffnen.
 
-Korrektur:
-Jede Speisekartenseite wird nun immer vollständig in das Popup eingepasst.
-Es gibt keinen Seitenbreite-Modus und kein vertikales Abschneiden mehr.
+Der Speisekarten-Viewer zeigt jede Seite vollständig an.
+Zoom ist per +/-, Tastatur, Strg/Cmd + Mausrad und auf dem iPhone per Zwei-Finger-Geste möglich.
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const CACHE="paris-reise-v5";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./carte-food-perruche-summer.pdf","./Elio.pdf","./menu-perruche-1.jpg","./menu-perruche-2.jpg","./menu-perruche-3.jpg","./menu-perruche-4.jpg","./menu-perruche-5.jpg","./menu-perruche-6.jpg","./menu-elio-1.jpg","./menu-elio-2.jpg","./menu-elio-3.jpg","./menu-elio-4.jpg"];
+const CACHE="paris-reise-v6";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./carte-food-perruche-summer.pdf","./Elio.pdf","./menu-perruche-1.jpg","./menu-perruche-2.jpg","./menu-perruche-3.jpg","./menu-perruche-4.jpg","./menu-perruche-5.jpg","./menu-perruche-6.jpg","./menu-elio-1.webp","./menu-elio-2.webp","./menu-elio-3.webp","./menu-elio-4.webp"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",event=>{
